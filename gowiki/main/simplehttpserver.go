57bde53b5c8a378e91b3128cf8d6f27051982e09
Marcos Maia <maia.marcos@gmail.com>
2017-07-01 15:23:54 +0200

    simple http server loading txt and handling request on root with a link to generated page.

--- gowiki/main/simplehttpserver.go
+++ gowiki/main/simplehttpserver.go
@@ -3,13 +3,21 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"github.com/mmaia/go-playground/gowiki"
 )
 
 func handler(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(responseWriter, "Hi there, I love %s!", request.URL.Path[1:])
+	fmt.Fprintf(responseWriter, "<p>Hi there, you're at the root, unfortunately there's nothing here, <a href=/view/TestPage> try this link instead. </a></p>")
+}
+
+func viewHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/view/"):]
+	p, _ := gowiki.LoadPage(title)
+	fmt.Fprintf(responseWriter, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
