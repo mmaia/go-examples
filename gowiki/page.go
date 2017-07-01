e750dda49b00540e012be0fc05e20252c24bf7f8
Marcos Maia <maia.marcos@gmail.com>
2017-07-01 14:06:19 +0200

    save and load page working, added build directory to host generated files and to .gitignore so they're not added by mistake

--- gowiki/page.go
+++ gowiki/page.go
@@ -1,6 +1,9 @@
 package gowiki
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"os"
+)
 
 type Page struct {
 	Title string
@@ -13,14 +16,16 @@ type Page struct {
 // return nil, if all goes well and the body of the page is successfully written to disk
 // return error, if something goes wrong
 func (page *Page) Save() error {
+	pwd, _ := os.Getwd()
 	filename := page.Title + ".txt"
-	return ioutil.WriteFile(filename, page.Body, 0600)
+	return ioutil.WriteFile(pwd + "/build/" + filename, page.Body, 0600)
 }
 
 // Returns a file from disk using the title parameter as the file name and returning it's content as a Page type.
 // Functions can return multiple values. The standard library function io.ReadFile returns []byte and error.
 func LoadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	pwd, _ := os.Getwd()
+	filename := pwd + "/build/" + title + ".txt"
 	body, err := ioutil.ReadFile(filename) // ReadFile returns []byte and error
 	if err != nil {
 		return nil, err
